21fa2811de12a092f853e8769b0244fa3bce1a7e
sss7526 <sss7526>
2026-05-06 15:17:08 -0400

    added colors

---
+++ color.go
@@ -0,0 +1,22 @@
+package resistor
+
+// Color represents a standard IEC resistor band color.
+type Color string
+
+const (
+	Black Color = "black"
+	Brown Color = "brown"
+	Red Color = "red"
+	Orange Color = "orange"
+	Yellow Color = "yellow"
+	Green Color = "green"
+	Blue Color = "blue"
+	Violet Color = "violet"
+	Grey Color = "grey"
+	White Color = "white"
+
+	Gold Color = "gold"
+	Silver Color = "silver"
+
+	None Color = "none" // used when no tolerance band exists (±20%)
+)
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/sss7526/resistor
+
+go 1.26.2
